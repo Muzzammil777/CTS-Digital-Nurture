82b02b578484e89a0227de26db945b768f38f8d6
Muzzammil777 <>
2026-06-18 21:39:18 +0530

    Week1 - Design Patterns and Principles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Logger obj1 = Logger.getInstance();
+        
+        Logger obj2 = Logger.getInstance();
+        System.out.println(obj1 == obj2);
+    }
+}
